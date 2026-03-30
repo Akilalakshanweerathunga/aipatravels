7b2ddec0974189ae547b12318ac37f74fbfa39e4
akilalakshanweerathunga <wbakilalakshan@gmail.com>
2026-03-30 13:00:33 +0530

    update

---
+++ src/app/[locale]/destinations/DestinationsClient.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import HeroBanner from '@/components/destination/HeroBanner';
+import DestinationSection from '@/components/destination/DestinationSection';
+import CustomJourney from '@/components/partials/CustomJourneyBanner';
+import { useTranslation } from 'react-i18next';
+export default function DestinationsClient() {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <HeroBanner
+        headTitle={t('destinations.headTitle')}
+        title={t('destinations.title')}
+        subtitle={t('destinations.subtitle')}
+        image="/images/hero/destination.jpg"
+      />
+
+      <DestinationSection />
+      <CustomJourney />
+    </>
+  );
+}
--- src/app/[locale]/destinations/page.tsx
+++ src/app/[locale]/destinations/page.tsx
@@ -1,22 +1,5 @@
-import HeroBanner from '@/components/destination/HeroBanner';
-import DestinationSection from '@/components/destination/DestinationSection';
-import CustomJourney from '@/components/partials/CustomJourneyBanner';
-import { useTranslation } from 'react-i18next';
+import DestinationsClient from './DestinationsClient';
 
 export default function DestinationsPage() {
-  const { t } = useTranslation();
-
-  return (
-    <>
-      <HeroBanner
-        headTitle={t('destinations.headTitle')}
-        title={t('destinations.title')}
-        subtitle={t('destinations.subtitle')}
-        image="/images/hero/destination.jpg"
-      />
-
-      <DestinationSection />
-      <CustomJourney />
-    </>
-  );
+  return <DestinationsClient />;
 }
